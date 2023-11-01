8685fea8b3e994c01a8927b5d0c99e3099888e4b
Tycon-2 <138762559+Tycon-2@users.noreply.github.com>
2023-11-02 00:50:04 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "Sushantrajhsb@gmail.com"
+var accountPassword = "12345"
+accountcity = "patna"
+let accountState;
+// accountId=2 //not allowed
+
+accountEmail="sadf@gmail.com"
+accountPassword="212121"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
